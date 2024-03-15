9e09815b89059ce4a16790a58126bc4fa8d7d350
Quan <doquan020903@gmail.com>
2024-03-16 02:07:02 +0700

    Add authorize to Axios Header

--- apps/frontend/actions/getSongs.ts
+++ apps/frontend/actions/getSongs.ts
@@ -2,22 +2,31 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 
 import { Song } from '@/types';
+import { getRequest } from '@/lib/fetch';
 
 const getSongs = async (): Promise<Song[]> => {
-  const supabase = createServerComponentClient({
-    cookies: cookies,
-  });
+    const onGetSongsByUserId = async () => {
+        const songs = await getRequest({
+            endPoint: `/api`,
+        });
+        console.log('🚀 ~ onGetSongsByUserId ~ songs:', songs);
+        return songs;
+    };
+    console.log('15:res: ' + JSON.stringify(onGetSongsByUserId()));
+    const supabase = createServerComponentClient({
+        cookies: cookies,
+    });
 
-  const { data, error } = await supabase
-    .from('songs')
-    .select('*')
-    .order('created_at', { ascending: false });
+    const { data, error } = await supabase
+        .from('songs')
+        .select('*')
+        .order('created_at', { ascending: false });
 
-  if (error) {
-    console.log(error.message);
-  }
+    if (error) {
+        console.log(error.message);
+    }
 
-  return (data as any) || [];
+    return (data as any) || [];
 };
 
 export default getSongs;
