9280ecce4550f60eee4dd62981eb5815410c529a
Quan <doquan020903@gmail.com>
2024-03-15 09:34:01 +0700

    init devQuan branch

---
+++ apps/backend/src/common/supabase/supabase.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable, Logger, Scope } from '@nestjs/common';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({ scope: Scope.REQUEST })
+export class Supabase {
+  private readonly logger = new Logger(Supabase.name);
+  private clientInstance: SupabaseClient;
+
+  constructor(
+    @Inject(REQUEST) private readonly request: Request,
+    private readonly configService: ConfigService,
+  ) {}
+
+  getClient() {
+    this.logger.log('getting supabase client...');
+    if (this.clientInstance) {
+      this.logger.log('client exists - returning for current Scope.REQUEST');
+      return this.clientInstance;
+    }
+
+    this.logger.log('initialising new supabase client for new Scope.REQUEST');
+
+    this.clientInstance = createClient(
+      this.configService.get('NEXT_PUBLIC_SUPABASE_URL'),
+      this.configService.get('NEXT_PUBLIC_SUPABASE_ANON_KEY'),
+    );
+
+    // this.clientInstance.auth.setAuth(
+    //   ExtractJwt.fromAuthHeaderAsBearerToken()(this.request),
+    // );
+    this.logger.log('auth has been set!');
+
+    return this.clientInstance;
+  }
+}
--- apps/frontend/components/AuthModal.tsx
+++ apps/frontend/components/AuthModal.tsx
@@ -1,8 +1,5 @@
 'use client';
-import {
-  useSessionContext,
-  useSupabaseClient,
-} from '@supabase/auth-helpers-react';
+import { useSessionContext, useSupabaseClient } from '@supabase/auth-helpers-react';
 import Modal from './Modal';
 import { useRouter } from 'next/navigation';
 import { Auth } from '@supabase/auth-ui-react';
@@ -11,50 +8,49 @@ import useAuthModal from '@/hooks/useAuthModal';
 import { useEffect } from 'react';
 
 const AuthModal = () => {
-  const supabaseClient = useSupabaseClient();
-  const router = useRouter();
-  const { session } = useSessionContext();
-  const { onClose, isOpen } = useAuthModal();
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const { session } = useSessionContext();
+    const { onClose, isOpen } = useAuthModal();
 
-  useEffect(() => {
-    if (session) {
-      router.refresh();
-      onClose();
-    }
-  }, [session, router, onClose]);
+    useEffect(() => {
+        if (session) {
+            router.refresh();
+            onClose();
+        }
+    }, [session, router, onClose]);
 
-  const onChange = (open: boolean) => {
-    if (!open) {
-      onClose();
-    }
-  };
+    const onChange = (open: boolean) => {
+        if (!open) {
+            onClose();
+        }
+    };
 
-  return (
-    <Modal
-      title="Welcome back"
-      description="Login to your account"
-      isOpen={isOpen}
-      onChange={onChange}
-    >
-      <Auth
-        theme="dark"
-        magicLink
-        providers={['github']}
-        supabaseClient={supabaseClient}
-        appearance={{
-          theme: ThemeSupa,
-          variables: {
-            default: {
-              colors: {
-                brand: '#404040',
-                brandAccent: '#22c55e',
-              },
-            },
-          },
-        }}
-      />
-    </Modal>
-  );
+    return (
+        <Modal
+            title="Welcome back"
+            description="Login to your account"
+            isOpen={isOpen}
+            onChange={onChange}
+        >
+            <Auth
+                theme="dark"
+                magicLink
+                supabaseClient={supabaseClient}
+                appearance={{
+                    theme: ThemeSupa,
+                    variables: {
+                        default: {
+                            colors: {
+                                brand: '#404040',
+                                brandAccent: '#22c55e',
+                            },
+                        },
+                    },
+                }}
+            />
+        </Modal>
+    );
 };
 
 export default AuthModal;
